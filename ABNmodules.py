9b360355df4a9a09d1dbeed9b9f2bcf56fdb5b76
yoojungsun0 <yoojungsun0@gmail.com>
2020-05-27 08:39:23 +0000

    change: added branch

--- ABNmodules.py
+++ ABNmodules.py
@@ -5,6 +5,53 @@ from tensorflow.keras import activations, Model, Input
 # from tensorflow.python.keras.layers.noise import GaussianNoise
 # from tensorflow.keras import GaussianNoise
 from tensorflow.keras.layers import GaussianNoise
+import tensorflow as tf
+
+def global_average_pooling(x):
+        return K.mean(x, axis = (2, 3))
+    
+def global_average_pooling_shape(input_shape):
+        return input_shape[0:2]
+    
+
+    
+def get_cam(args):
+    x = args[0]
+    class_weights = args[1]
+    target_classes=args[2]
+#     x, class_weights, target_classes
+#     cam = K.zeros(x.shape)
+    cam=[]
+    for curr_class in target_classes:
+        for i, w in enumerate(class_weights[:, curr_class]):
+            cam += w * x[:,i,:]
+    cam %= len(target_classes)
+    return cam
+    
+def CAM_branch(x, n_classes, minimum_len, target_classes, name='cam_branch'): 
+    # x is the output image from last convolutional layer
+    
+    out = layers.GlobalAveragePooling1D(name=name+'_avgpool_1')(x)
+    
+    out = layers.Dense(n_classes)(out)
+    out = layers.Softmax(out)
+    class_weights = layers.Layer.get_weights(out)
+    cam_out = layers.Lambda(get_cam)([x, class_weights, target_classes])
+    
+
+    
+    
+    return final_out, cam_out
+#     out = layers.Lambda(global_average_pooling)(
+#     out = global_average_pooling(x)
+# att_out = layers.Lambda(lambda z: (z[0] * z[1]) + z[0])([x, att_out])
+
+
+#     out = layers.GlobalAveragePooling1D(name=name+'_avgpool_1')(out)
+#     out = layers.Dense(256, name=name+'_dense_1')(out)
+#     out = layers.Dense(n_classes, name=name+'_dense_2')(out)
+#     return layers.Softmax(name='perception_branch_output')(out)
+
 def ieee_baseline_network(x):
     bn_axis=2
     ep = 1.001e-5
@@ -22,21 +69,15 @@ def ieee_baseline_network(x):
     out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)
     
     # Block 2
-    out = layers.Conv1D(256, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(256, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(256, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
+    for _ in range(3):
+        out = layers.Conv1D(256, 3, 1, 'same')(out)
+        out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
     out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)
     
     # Block 3
-    out = layers.Conv1D(512, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(512, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(512, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
+    for _ in range(3):
+        out = layers.Conv1D(512, 3, 1, 'same')(out)
+        out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
     out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)    
     
     # Block 4
@@ -50,9 +91,7 @@ def ieee_baseline_network(x):
     
     return out
 
-# pred_out = layers.GlobalAveragePooling1D(name=name+'_gap_1')(pred_out)
-# tf.keras.layers.MaxPooling1D(
-#     pool_size=2, strides=None, padding="valid", data_format="channels_last", **kwargs)
+
 def basic_block(x, out_ch, kernel_size=3, stride=1, last_act=True):
     """
     (batch, height, width, channels) => (batch, heigth, width, out_ch)
@@ -138,8 +177,18 @@ def attention_branch(x, n, n_classes, name='attention_branch'):
     return pred_out, att_out
 
 
-def perception_branch(x, n, n_classes, name='perception_branch'):
+def perception_branch(x, cam_map, n, n_classes, name='perception_branch'):
+    cam_map = tf.image.resize(cam_map, x.shape)
+    filters = cam_map + x
+#     cam_map = tf.image.resize(cam_map, filters._shape_tuple())
+    
+    
     filters = x._shape_tuple()[-1]
+#     filters_cam = cam_map.__shape_tuple
+#     cam_map.reshape(
+    
+    
+    
     out = bottleneck_block(x, filters*2, 1)
     for _ in range(n-1):
         out = bottleneck_block(out, filters*2, 1)
@@ -163,14 +212,15 @@ def get_model(input_shape, n_classes, out_ch=256, n=18):
     return model
 
 
-def get_custom_model(input_shape, n_classes, out_ch=256, n=18):
+def get_custom_model(input_shape, n_classes, minimum_len, target_classes, out_ch=256, n=18):
     img_input = Input(shape=input_shape, name='input_image')
 #     img_input = tensorflow.keras.layers.GaussianNoise(0.01)(img_input) # YJS added
 #     img_input = GaussianNoise(0.01)(img_input)
 #     backbone = feature_extractor(img_input, out_ch, n)
     backbone = ieee_baseline_network(img_input)
     att_pred, att_map = attention_branch(backbone, n, n_classes)
-    per_pred = perception_branch(att_map, n, n_classes)
-
-    model = Model(inputs=img_input, outputs=[att_pred, per_pred])
+    cam_pred, cam_map = CAM_branch(backbone, n_classes, minimum_len, target_classes)
+    per_pred = perception_branch(att_map, cam_map, n, n_classes)
+    model = Model(inputs=img_input, outputs=[att_pred, per_pred, cam_pred])
     return model
+
