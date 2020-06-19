4a59941d1227951bfb7bdd7325a341753dc4ec02
yoojungsun0 <yoojungsun0@gmail.com>
2020-06-19 08:23:46 +0000

    cleansed code and added f1 in validation

--- ABNmodules_multiclass.py
+++ ABNmodules_multiclass.py
@@ -1,88 +1,11 @@
 from tensorflow.keras import layers
 from tensorflow.keras import backend as K
 from tensorflow.keras import activations, Model, Input
-# tensorflow.python.keras.layers.noise.GaussianNoise
-# from tensorflow.python.keras.layers.noise import GaussianNoise
-# from tensorflow.keras import GaussianNoise
 from tensorflow.keras.layers import GaussianNoise
 import tensorflow as tf
-# import tensorflow.contrib.eager as tfe
 import cProfile
-# tf.executing_ea
 from keras import initializers
 from keras.backend import tf as ktf
-
-
-
-
-def global_average_pooling(x):
-        return K.mean(x, axis = (2, 3))
-    
-def global_average_pooling_shape(input_shape):
-        return input_shape[0:2]
-    
-
-    
-def get_cam(args):
-    x = args[0]
-    class_weights = args[1]
-    target_classes=args[2]
-#     x, class_weights, target_classes
-#     cam = K.zeros(x.shape)
-    cam=[]
-    for curr_class in target_classes:
-        for i, w in enumerate(class_weights[:, curr_class]):
-            cam += w * x[:,i,:]
-    cam %= len(target_classes)
-    return cam
-
-
-class CAM_dense(layers.Layer):
-    def __init__(self, units=9):
-        super(CAM_dense, self).__init__()
-        self.units = units
-        
-    def build(self, input_shape):
-        self.w = self.add_weight(shape=(input_shape[-1], self.units), 
-            initializer=initializers.get('glorot_uniform'), 
-            trainable=True)
-        self.b = self.add_weight(shape=(self.units,),
-            initializer = initializers.get('glorot_uniform'), 
-            trainable=True)
-        super(CAM_dense, self).build(input_shape)
-
-    def call(self, inputs):
-        out = tf.matmul(inputs, self.w) + self.b
-        return out
-    def weights(self):
-        weight = self.w
-        return weight
-
-    
-def CAM_branch(x, n_classes, minimum_len, target_classes, name='cam_branch'): 
-    # x is the output image from last convolutional layer
-    
-    out = layers.GlobalAveragePooling1D(name=name+'_avgpool_1')(x)
-    out = CAM_dense(out)
-    w = out.w
-#     print(w)
-#     out = layers.Dense(n_classes)(out)
-    final_out = layers.Softmax(out)
-
-
-#     class_weights = final_out.get_weights()#layers.Layer.get_weights(final_out)
-    cam_out = tf.matmul(x, w)#layers.Lambda(lambda z: z[0] * z[1])([x,w])
-#     cam_out = layers.Lambda(get_cam)([x, w, 2])
-
-    return final_out, cam_out
-
-#     att_out = layers.Lambda(lambda z: (z[0] * z[1]) + z[0])([x, att_out])
-
-
-
-
-
-
 
 def ieee_baseline_network(x):
     bn_axis=2
@@ -122,46 +45,6 @@ def ieee_baseline_network(x):
     out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)    
     
     return out
-
-def ieee_baseline_network_2(x):
-    bn_axis=2
-    ep = 1.001e-5
-    
-    # Block 1
-    out = layers.Conv1D(64, 3, 1, 'same')(x)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(64, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)
-    out = layers.Conv1D(128, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(128, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)
-    
-    # Block 2
-    for _ in range(3):
-        out = layers.Conv1D(256, 3, 1, 'same')(out)
-        out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)
-    
-    # Block 3
-    for _ in range(3):
-        out = layers.Conv1D(512, 3, 1, 'same')(out)
-        out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)    
-    
-    # Block 4
-    out = layers.Conv1D(512, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(256, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.Conv1D(128, 3, 1, 'same')(out)
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep)(out)
-    out = layers.MaxPooling1D(pool_size=3, strides=3, padding='same')(out)    
-    
-    return out
-
 
 def basic_block(x, out_ch, kernel_size=3, stride=1, last_act=True):
     """
@@ -218,6 +101,32 @@ def feature_extractor(x, out_ch, n):
         out = bottleneck_block(out, out_ch*2)
     return out
 
+def attention_branch(x, n, n_classes, name='attention_branch'): # heatmap 없는 원래 버전
+    """
+    (batch, height, width, channels) =>
+        (batch, n_classes), (batch, height, width, channels)
+    """
+#     bn_axis = 3 if K.image_data_format() == 'channels_last' else 1
+    bn_axis=2
+    ep = 1.001e-5
+
+    filters = x._shape_tuple()[-1]
+    out = bottleneck_block(x, filters*2, 1) # (b,h/2,w/2,f*2*4*2)
+    for _ in range(n-1):
+        out = bottleneck_block(out, filters*2, 1) # (b,h/2,w/2,f*2*4*2)
+
+    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep, name=name+'_bn_1')(out)
+    out = layers.Conv1D(n_classes, 1, 1, 'same', use_bias=False, activation=activations.relu ,name=name+'_conv_1')(out)
+
+    pred_out = layers.Conv1D(n_classes, 1, 1, 'same', use_bias=False, name=name+'_pred_conv_1')(out)
+    pred_out = layers.GlobalAveragePooling1D(name=name+'_gap_1')(pred_out)
+    pred_out = layers.Activation(activations.sigmoid, name='attention_branch_output')(pred_out)
+    
+    att_out = layers.Conv1D(1, 1, 1, 'same', use_bias=False, name=name+'_att_conv_1')(out)
+    att_out = layers.BatchNormalization(axis=bn_axis, epsilon=ep, name=name+'_att_bn_1')(att_out)
+    att_out = layers.Activation(activations.sigmoid, name=name+'_att_sigmoid_1')(att_out)
+    att_out = layers.Lambda(lambda z: (z[0] * z[1]) + z[0])([x, att_out])
+    return pred_out, att_out
 
 def attention_branch_edit(x, n, n_classes, heatmap, name='attention_branch'):
     """
@@ -238,24 +147,11 @@ def attention_branch_edit(x, n, n_classes, heatmap, name='attention_branch'):
 
     pred_out = layers.Conv1D(n_classes, 1, 1, 'same', use_bias=False, name=name+'_pred_conv_1')(out)
     pred_out = layers.GlobalAveragePooling1D(name=name+'_gap_1')(pred_out)
-#     pred_out = layers.Softmax(name='attention_branch_output')(pred_out)
     pred_out = layers.Activation(activations.sigmoid, name='attention_branch_output')(pred_out)
 
     att_out = layers.Conv1D(1, 1, 1, 'same', use_bias=False, name=name+'_att_conv_1')(out)
     att_out = layers.BatchNormalization(axis=bn_axis, epsilon=ep, name=name+'_att_bn_1')(att_out)
     att_out = layers.Activation(activations.sigmoid, name=name+'_att_sigmoid_1')(att_out)  # output shape = (batch, 12, 1)
-    
-    # x shape = (batch, 12, 128)
-    
-    
-#     print(att_out.shape)
-#     print(heatmap.shape)
-    # att_out = (x * att_out) + x
-    
-    # heatmap 여기에서 추가
-#     editted = layers.concatenate([att_out, heatmap], axis=1)
-#     editted = layers.Lambda(lambda z: (z[0] + z[1])/2)([att_out, heatmap])
-#     edit_map = layers.Lambda(lambda z: (z[0] * z[1]) + z[0])([x, editted])
 
     att_out = layers.Lambda(lambda z: (z[0] * z[1]) + z[0])([x, att_out]) # 다음에 돌릴땐 heatmap을 이거 전에 넣어주는것도 고려해볼만할듯?
     heat_out = layers.Lambda(lambda z: (z[0] * z[1]) + z[0])([x, heatmap])
@@ -266,48 +162,9 @@ def attention_branch_edit(x, n, n_classes, heatmap, name='attention_branch'):
     return pred_out, edit_map
 
 
-def attention_branch2(x, n, n_classes, name='attention_branch'): # heatmap 없는 원래 버전
-    """
-    (batch, height, width, channels) =>
-        (batch, n_classes), (batch, height, width, channels)
-    """
-#     bn_axis = 3 if K.image_data_format() == 'channels_last' else 1
-    bn_axis=2
-    ep = 1.001e-5
-
-    filters = x._shape_tuple()[-1]
-    out = bottleneck_block(x, filters*2, 1) # (b,h/2,w/2,f*2*4*2)
-    for _ in range(n-1):
-        out = bottleneck_block(out, filters*2, 1) # (b,h/2,w/2,f*2*4*2)
-
-    out = layers.BatchNormalization(axis=bn_axis, epsilon=ep, name=name+'_bn_1')(out)
-    out = layers.Conv1D(n_classes, 1, 1, 'same', use_bias=False, activation=activations.relu ,name=name+'_conv_1')(out)
-
-    pred_out = layers.Conv1D(n_classes, 1, 1, 'same', use_bias=False, name=name+'_pred_conv_1')(out)
-    pred_out = layers.GlobalAveragePooling1D(name=name+'_gap_1')(pred_out)
-    pred_out = layers.Activation(activations.sigmoid, name='attention_branch_output')(pred_out)
-#     pred_out = layers.Softmax(name='attention_branch_output')(pred_out)
-    
-
-    att_out = layers.Conv1D(1, 1, 1, 'same', use_bias=False, name=name+'_att_conv_1')(out)
-    att_out = layers.BatchNormalization(axis=bn_axis, epsilon=ep, name=name+'_att_bn_1')(att_out)
-    att_out = layers.Activation(activations.sigmoid, name=name+'_att_sigmoid_1')(att_out)
-    # att_out = (x * att_out) + x
-    att_out = layers.Lambda(lambda z: (z[0] * z[1]) + z[0])([x, att_out])
-    return pred_out, att_out
-
 def perception_branch(x,n, n_classes, name='perception_branch'):
-#     cam_map = tf.image.resize(cam_map, x.shape)
-#     filters = cam_map + x
-#     cam_map = tf.image.resize(cam_map, filters._shape_tuple())
-    
-    
+
     filters = x._shape_tuple()[-1]
-#     filters_cam = cam_map.__shape_tuple
-#     cam_map.reshape(
-    
-    
-    
     out = bottleneck_block(x, filters*2, 1)
     for _ in range(n-1):
         out = bottleneck_block(out, filters*2, 1)
@@ -316,42 +173,10 @@ def perception_branch(x,n, n_classes, name='perception_branch'):
     out = layers.Dense(512, name=name+'_dense_1')(out)
     out = layers.Dense(n_classes, name=name+'_dense_2')(out)
     return layers.Activation(activations.sigmoid, name='perception_branch_output')(out)
-#     return layers.Softmax(name='perception_branch_output')(out)
 
-def perception_branch2(x,n, n_classes, name='perception_branch'):
-#     cam_map = tf.image.resize(cam_map, x.shape)
-#     filters = cam_map + x
-#     cam_map = tf.image.resize(cam_map, filters._shape_tuple())
-    
-    
+def perception_branch_primitive(x,n, n_classes, name='perception_branch'):
+    # 특징: same as perception_branch but no 512 dense layer after GAP (for CAM afterwards)
     filters = x._shape_tuple()[-1]
-#     filters_cam = cam_map.__shape_tuple
-#     cam_map.reshape(
-    
-    
-    
-    out = bottleneck_block(x, filters*2, 1)
-    for _ in range(n-1):
-        out = bottleneck_block(out, filters*2, 1)
-
-    out = layers.GlobalAveragePooling1D(name=name+'_avgpool_1')(out)
-    out = layers.Dense(512, name=name+'_dense_1')(out)
-    out = layers.Dense(n_classes, name=name+'_dense_2')(out)
-    return layers.Activation(activations.sigmoid, name='perception_branch_output')(out)
-#     return layers.Softmax(name='perception_branch_output')(out)
-
-def perception_branch3(x,n, n_classes, name='perception_branch'):
-#     cam_map = tf.image.resize(cam_map, x.shape)
-#     filters = cam_map + x
-#     cam_map = tf.image.resize(cam_map, filters._shape_tuple())
-    
-    
-    filters = x._shape_tuple()[-1]
-#     filters_cam = cam_map.__shape_tuple
-#     cam_map.reshape(
-    
-    
-    
     out = bottleneck_block(x, filters*2, 1)
     for _ in range(n-1):
         out = bottleneck_block(out, filters*2, 1)
@@ -360,70 +185,58 @@ def perception_branch3(x,n, n_classes, name='perception_branch'):
 #     out = layers.Dense(512, name=name+'_dense_1')(out)
     out = layers.Dense(n_classes, name=name+'_dense_2')(out)
     return layers.Activation(activations.sigmoid, name='perception_branch_output')(out)
-#     return layers.Softmax(name='perception_branch_output')(out)
 
-def get_model(input_shape, n_classes, out_ch=256, n=18):
+
+def primitive_ABN(input_shape, n_classes, minimum_len, target_classes, out_ch=256, n=18):
+    # use for training ABN for ABN (extract CAM from this model later)
     img_input = Input(shape=input_shape, name='input_image')
-#     img_input = GaussianNoise(0.01)(img_input) # YJS added
-#     img_input = GaussianNoise(0.01)(img_input)
-    backbone = feature_extractor(img_input, out_ch, n)
-#     backbone = ieee_baseline_network(img_input, n)
+    backbone = ieee_baseline_network(img_input)
+    att_pred, att_map = attention_branch(backbone, n, n_classes)
+    per_pred = perception_branch_primitive(att_map, n, n_classes)
+    model = Model(inputs=img_input, outputs=[att_pred, per_pred])
+    return model
+
+def ABN_model(input_shape, n_classes, minimum_len, target_classes, out_ch=256, n=18):
+    img_input = Input(shape=input_shape, name='input_image')
+    backbone = ieee_baseline_network(img_input)
     att_pred, att_map = attention_branch(backbone, n, n_classes)
     per_pred = perception_branch(att_map, n, n_classes)
-
     model = Model(inputs=img_input, outputs=[att_pred, per_pred])
     return model
 
-
-def get_ABN_backbone(input_shape, n_classes, minimum_len, target_classes, out_ch=256, n=18):
-    img_input = Input(shape=input_shape, name='input_image')
-#     img_input = tensorflow.keras.layers.GaussianNoise(0.01)(img_input) # YJS added
-#     img_input = GaussianNoise(0.01)(img_input)
-#     backbone = feature_extractor(img_input, out_ch, n)
-    backbone = ieee_baseline_network(img_input)
-    att_pred, att_map = attention_branch2(backbone, n, n_classes)
-#     cam_pred, cam_map = CAM_branch(backbone, n_classes, minimum_len, target_classes)
-    per_pred = perception_branch3(att_map, n, n_classes)
-    model = Model(inputs=img_input, outputs=[att_pred, per_pred])
-    return model
-
-
-def get_custom_model(input_shape, n_classes, minimum_len, target_classes, out_ch=256, n=18):
-    img_input = Input(shape=input_shape, name='input_image')
-#     img_input = tensorflow.keras.layers.GaussianNoise(0.01)(img_input) # YJS added
-#     img_input = GaussianNoise(0.01)(img_input)
-#     backbone = feature_extractor(img_input, out_ch, n)
-    backbone = ieee_baseline_network(img_input)
-    att_pred, att_map = attention_branch2(backbone, n, n_classes)
-#     cam_pred, cam_map = CAM_branch(backbone, n_classes, minimum_len, target_classes)
-    per_pred = perception_branch(att_map, n, n_classes)
-    model = Model(inputs=img_input, outputs=[att_pred, per_pred])
-    return model
-
-def cam_model(input_shape, n_classes, minimum_len, out_ch=256, n=18):
-    img_input = Input(shape=input_shape, name='input_image')
-    backbone = ieee_baseline_network(img_input)
-    
-    x = layers.GlobalAveragePooling1D()(backbone)
-    x = layers.Dense(256, activation=None)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation(activation='relu')(x)
-    x = layers.Dense(256, activation=None)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation(activation='relu')(x)
-    x = layers.Dense(n_classes, name='dense_final')(x)
-    out = layers.Softmax(name='output')(x)        
-
-    model = Model(inputs=img_input, outputs=out)
-    return model
-
-
-def edit_model(input_shape, n_classes, minimum_len, out_ch=256, n=1): 
+def edit_ABN_model(input_shape, n_classes, minimum_len, out_ch=256, n=1): 
     img_input = Input(shape=input_shape, name='input_image')
     
     backbone = ieee_baseline_network(img_input)
     heatmap = Input(shape=(None,1), name='heatmap_image')    
     att_pred, edit_map = attention_branch_edit(backbone, n, n_classes, heatmap)
-    per_pred = perception_branch2(edit_map, n, n_classes)
+    per_pred = perception_branch(edit_map, n, n_classes)
     model = Model(inputs=[img_input, heatmap], outputs=[att_pred, per_pred])
     return model
+
+# def cam_primitive_model(input_shape, n_classes, minimum_len, out_ch=256, n=18): # don't use as backbone anymore (bad performance)
+#     img_input = Input(shape=input_shape, name='input_image')
+#     backbone = ieee_baseline_network(img_input)
+    
+#     x = layers.GlobalAveragePooling1D()(backbone)
+#     x = layers.Dense(256, activation=None)(x)
+#     x = layers.BatchNormalization()(x)
+#     x = layers.Activation(activation='relu')(x)
+#     x = layers.Dense(256, activation=None)(x)
+#     x = layers.BatchNormalization()(x)
+#     x = layers.Activation(activation='relu')(x)
+#     x = layers.Dense(n_classes, name='dense_final')(x)
+#     out = layers.Softmax(name='output')(x)        
+
+#     model = Model(inputs=img_input, outputs=out)
+#     return model
+
+# def get_model(input_shape, n_classes, out_ch=256, n=18):
+#     # 원본 ABN model 
+#     img_input = Input(shape=input_shape, name='input_image')
+#     backbone = feature_extractor(img_input, out_ch, n)
+#     att_pred, att_map = attention_branch(backbone, n, n_classes)
+#     per_pred = perception_branch(att_map, n, n_classes)
+
+#     model = Model(inputs=img_input, outputs=[att_pred, per_pred])
+#     return model
